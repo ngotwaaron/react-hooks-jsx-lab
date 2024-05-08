aec56f7e30529f29cd8fc42c591025922c0d38d0
ngotwaaron <ngotwaaron@gmail.com>
2024-05-08 11:24:22 +0300

    complete react hooks jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+    <h2>About me</h2>
+    <p>Sylvia Chebet is a Geospatial Engineering graduate and an aspiring Full Stack Web Developer</p>
+   <img src={image} alt="I made this"></img>
+   </div>
+  )
 }
 
 export default About;
